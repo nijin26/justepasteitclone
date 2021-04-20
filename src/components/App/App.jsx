b5e3be2e796d3fe182186adf7d5c337446ed0f69
Nijin-26 <nijinazarkpz@gmail.com>
2021-04-20 11:33:48 +0530

    Fixed Routing with useHistory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,70 +1,51 @@
-import React,{useState} from 'react'
-import Header from '../Header/Header'
-import ShareButton from '../ShareButton/ShareButton'
-import Typing from '../Typing/Typing'
-import axios from 'axios'
-import { Redirect } from 'react-router-dom';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-import SharePage from '../SharePage/SharePage'
+import React, { useState } from "react";
+import Header from "../Header/Header";
+import ShareButton from "../ShareButton/ShareButton";
+import Typing from "../Typing/Typing";
+import axios from "axios";
+import { Route, Switch, useHistory, withRouter } from "react-router-dom";
+import SharePage from "../SharePage/SharePage";
+
+import style from "./App.module.css";
 
 const App = () => {
+  const history = useHistory();
+  const [shareText, setShareText] = useState("");
+  //   const [routeNav, setRouteNav] = useState(false);
 
- const [shareText, setShareText] = useState('');
- const [routeNav, setRouteNav] = useState(false);
+  const handleInputChange = (inputValue) => {
+    setShareText(inputValue);
+    console.log(shareText);
+  };
 
+  const onSubmit = (inputValue) => {
+    history.push("/test");
+    console.log("pressed dude");
+    console.log(shareText);
+    axios
+      .post("http://localhost:2000/add", { content: shareText })
+      .then((response) => {
+        let id = response.data["_id"];
+        console.log(id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <Header headTitle="JustPasteit" />
+      <Switch>
+        <Route exact path="/">
+          <Typing handleInputChange={handleInputChange} />
+        </Route>
+        <Route exact path="/test">
+          <SharePage />
+        </Route>
+      </Switch>
+      <ShareButton onSubmit={onSubmit} />
+    </div>
+  );
+};
 
-    const handleInputChange = (inputValue)=>{
-        setShareText(inputValue)
-        console.log(shareText);
-    }
-
-    const onSubmit = (inputValue)=>{
-        console.log("pressed dude");
-        console.log(shareText);
-        axios.post('http://localhost:2000/add',{'content':shareText})
-        .then(response=>{
-
-
-            let id =response.data['_id'];
-            setRouteNav(true);
-            console.log(id);
-            console.log(routeNav)
-
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-
-
-
-        if(routeNav===true)
-        {
-        return <Redirect to='/test'/>
-
-        }
-
- 
-    }
-       return (
-        <Router>
-
-           <div>
-               <Header headTitle="JustPasteit"/>
-
-                   <Switch>
-                   <Route exact path='/'>
-                       <Typing handleInputChange={handleInputChange}/>
-                       </Route>
-                       <Route exact path='/test'>
-                    <SharePage/>
-                       </Route>
-                   </Switch>
- 
-               <ShareButton onSubmit={onSubmit} />
-           </div>
-           </Router>
-
-       )
-}
-
-export default App
+export default withRouter(App);
