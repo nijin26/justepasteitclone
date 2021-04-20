511a9b56a2743b657568c4b601d25314e87933b4
Nijin-26 <nijinazarkpz@gmail.com>
2021-04-20 11:19:02 +0530

    Typing Component style changed to Module

--- src/components/Typing/Typing.jsx
+++ src/components/Typing/Typing.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import './Typing.css'
-const Typing = ({handleInputChange}
-    ) => {
-    return (
-        <div>
-            <textarea onChange = {(e)=> handleInputChange(e.target.value) } className="textarea">
-            </textarea>
-            
-        </div>
-    )
-}
+import React from "react";
+import style from "./Typing.module.css";
+const Typing = ({ handleInputChange }) => {
+  return (
+    <div>
+      <textarea
+        onChange={(e) => handleInputChange(e.target.value)}
+        className={style.textarea_container}
+      ></textarea>
+    </div>
+  );
+};
 
-export default Typing
+export default Typing;
